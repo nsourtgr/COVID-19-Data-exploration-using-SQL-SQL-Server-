029c3473ed79cb538e4eec9e3c556f9b1faa1ace
Nikos <61462314+nsourtgr@users.noreply.github.com>
2023-03-27 09:49:38 +0300

    Add files via upload

--- Cov19 SQLQuery.sql
+++ Cov19 SQLQuery.sql
@@ -23,13 +23,58 @@ The columns from original table imported to SQL Server in table [cov19vaccinatio
       ,[total_vaccinations],[people_vaccinated],[people_fully_vaccinated],[new_vaccinations]
 */
 
+/* COVID-19 Data exploration List
+Data about Deaths, infections
+ - Find how many and the day that each Country has the first case(s) of covid-19 
+ - Find how many and the day that each Country has the first death case(s) of covid-19 
+ - Find how many and the day that each Country has the MAX new case(s) of covid-19 
+ - Find how many and the day that each Country has the MAX new deaths of covid-19 
+ - Find Total deaths per continent
+ - Highest deaths per location
+ - Find World Totals  
+ - Find Continents Totals  
+ - Checking Coronavirus (COVID-19) Death percentage over total Population 
+ - Find out the countries with the highest infection rates compared to Population 
+
+Data about Vaccinations
+ - Find out the countries with the highest infection rates compared to Population 
+ - Find how many and the day that each Country had the first people vaccinated
+ - Find how many and the day that each Country had the MAX new vaccinations
+ - Find how many and the day that each Country had the MAX new tests
+ - Highest total_vaccinations per continent
+ - Highest people_fully_vaccinated per  continent
+ - Highest total_vaccinations per location
+ - Vaccination Progress by Country over Population (VPOP) 
+ - Vaccinated People & Fully Vaccinated People Increment
+ - Percentage of Population that has been Vaccinated OR Fully Vaccinated
+
+Checking Data
+- Checking the data, I found difference, data not matching between Total Vaccinations by Date and Increment New Vaccinations by Date
+
+Greece Data Exploration
+ - FindCoronavirus (COVID-19) Death percentage over total covid19 cases
+and  Infection percentage over total population for Greece 
+ - Find  how many and the day that Greece has the first case(s) of covid-19 
+ - Find  how many and the day that Greece has the first death case(s) of covid-19 
+ - Find  how many and the day that Greece has the MAX new case(s) of covid-19 
+ - Find how many and the day thatGreece has the MAX new deaths of covid-19 
+ - Vaccination Progress in Greece over Population (VPOP)
+ - Vaccinated People & Fully Vaccinated People Increment
+ - Percentage of Population that has been Vaccinated OR Fully Vaccinated 
+*/ 
+
+
+
+
 /* Checking the data in both tables*/
 SELECT  *
   FROM [Covid19].[dbo].[cov19deaths]
+  where Date = '2022/02/28'
   order by continent, location, date;
 
   SELECT  *
   FROM [Covid19].[dbo].[cov19vaccinations]
+  where Date = '2022/02/28'
     order by continent, location, date;
 
 	-----------------------------------------
@@ -93,7 +138,8 @@ WITH added_row_number AS (
 SELECT
   *
 FROM added_row_number
-WHERE row_number = 1;
+WHERE row_number = 1
+order by FIRST_COVID_CASE desc;
 --==========================================
 
 /* Find  how many and the day that each Country has the first death case(s) of covid-19 */
@@ -104,11 +150,12 @@ WITH added_row_number AS (
     ROW_NUMBER() OVER(PARTITION BY [location] ORDER BY total_deaths ASC) AS row_number
   FROM [Covid19].[dbo].[cov19deaths]
   WHERE continent <> '' and total_deaths > 0
-)
+  )
 SELECT
   *
 FROM added_row_number
-WHERE row_number = 1;
+WHERE row_number = 1
+order by FIRST_COVID_DEATHS desc;
 --==========================================
 
 /* Find  how many and the day that each Country has the MAX new case(s) of covid-19 */
@@ -123,8 +170,8 @@ WITH added_row_number AS (
 SELECT
   *
 FROM added_row_number
-WHERE row_number = 1;
-
+WHERE row_number = 1
+order by MAX_COVID_NEW_CASE desc;
 --==========================================
 
 /* Find how many and the day that each Country has the MAX new deaths of covid-19 */
@@ -139,17 +186,17 @@ WITH added_row_number AS (
 SELECT
   *
 FROM added_row_number
-WHERE row_number = 1;
-
-
+WHERE row_number = 1
+order by MAX_COVID_NEW_DEATHS desc;
 --==========================================
+
+
 /* Highest deaths per  continent*/
 SELECT MAX(total_deaths) as TotalDeathNum, continent
 FROM [Covid19].[dbo].[cov19deaths]
 WHERE continent <> ''
 GROUP BY continent
 ORDER BY TotalDeathNum desc;
-
 --==========================================
 
 /* Highest deaths per location*/
@@ -193,9 +240,9 @@ SELECT
   *
 FROM added_row_number
 WHERE row_number = 1;
+--==========================================
 
-
-/* Find Continets Totals  */
+/* Find Continents Totals  */
 Select continent, MAX(Total_deaths) as TotalDeathCount
 FROM [Covid19].[dbo].[cov19deaths]
 Where continent is not null 
@@ -215,11 +262,59 @@ FROM added_row_number
 WHERE row_number = 1;
 --===================================================
 
+/* Checking Coronavirus (COVID-19) Death percentage over total Population */ 
+WITH added_row_number AS (
+  SELECT
+    [iso_code] ,[continent],[location],[date] as DATE_MAX_COVID_DEATHS,[population],
+	[total_cases],[new_cases], [total_deaths] as MAX_COVID_DEATHS ,[new_deaths],
+    ROW_NUMBER() OVER(PARTITION BY [location] ORDER BY [total_deaths] DESC) AS row_number
+  FROM [Covid19].[dbo].[cov19deaths]
+  WHERE location <> '' and new_cases > 0
+)
+SELECT
+  *, round((MAX_COVID_DEATHS/POPULATION) * 100,3) AS PercDeathsOverPoulatuon
+FROM added_row_number
+WHERE row_number = 1
+order by PercDeathsOverPoulatuon desc;
+ --==================     OR  
+
+ Select Location, Population, MAX([total_deaths]) as MAX_COVID_DEATHS,  round(Max(([total_deaths]/population))*100,4) as PercDeathsOverPoulatuon
+ FROM [Covid19].[dbo].[cov19deaths]
+Group by Location, Population
+order by PercDeathsOverPoulatuon desc
+ --========================================
+ 
+/* Find out the countries with the highest infection rates compared to Population */ 
+Select Location, Population, MAX(total_cases) as MaxInfectionCount,  round(Max((total_cases/population))*100,4) as PopulationInfectedPercent
+ FROM [Covid19].[dbo].[cov19deaths]
+Group by Location, Population
+order by PopulationInfectedPercent desc
+
+
+
+
+
 /*==================================================
 =====================================================              
 			   Data about Vaccinations
 ====================================================
 ====================================================*/
+
+/* Find out the countries with the highest infection rates compared to Population */ 
+Select [location],[date],
+    min([people_vaccinated])
+
+ FROM  [Covid19].[dbo].[cov19vaccinations]
+where [people_vaccinated]>0
+Group by Location, [date]
+order by Location desc
+
+
+ Select Location, Population, MAX([total_deaths]) as MAX_COVID_DEATHS,  round(Max(([total_deaths]/population))*100,4) as PercDeathsOverPoulatuon
+ FROM [Covid19].[dbo].[cov19deaths]
+Group by Location, Population
+order by PercDeathsOverPoulatuon desc
+
 
 /* Find how many and the day that each Country had the first people vaccinated*/
 WITH added_row_number AS (
@@ -290,6 +385,7 @@ FROM [Covid19].[dbo].[cov19vaccinations]
 WHERE continent <> ''
 GROUP BY location
 ORDER BY TotalVaccinationsNum desc; 
+
 
 --=========================Vaccination Progress by Country over Population (VPOP)===========================
 --=========================== Vaccinated People & Fully Vaccinated People Increment=========================
@@ -410,11 +506,20 @@ FROM [Covid19].[dbo].[cov19vaccinations]
 WHERE location = 'Greece'
 GROUP BY location;
 
-/* Highest people_fully_vaccinated per  continent*/
+/* Highest people_fully_vaccinated Greece*/
 SELECT MAX(people_fully_vaccinated) as TotalPeopleFullyVacNum, location
 FROM [Covid19].[dbo].[cov19vaccinations]
 WHERE location = 'Greece'
 GROUP BY location;
+
+
+WITH CTE AS (
+SELECT MAX(people_fully_vaccinated) as TotalPeopleFullyVacNum, location
+FROM [Covid19].[dbo].[cov19vaccinations]
+WHERE location = 'Greece'
+GROUP BY location)
+SELECT *
+FROM CTE;
 
 
 --=========================Vaccination Progress in Greece over Population (VPOP)===========================
@@ -435,7 +540,7 @@ Select *, ROUND((Total_People_vaccinated/Population)*100,4) as Perc_Total_People
 From VPOP;
 
 /*============== Checking Data
-Checking the data I found difference, data not matching between Total Vaccinations by Date and Increment New Vaccinations by Date
+Checking the data, I found difference, data not matching between Total Vaccinations by Date and Increment New Vaccinations by Date
 */
 With VPOP (Continent, Location, Date, Population, New_Vaccinations, Total_Vaccinations, VaccinationsIncrement)
 as
@@ -446,14 +551,17 @@ Select dt.continent, dt.location, dt.date, dt.population, vc.new_vaccinations, v
 Join [Covid19].[dbo].[cov19vaccinations] vc
 	On dt.location = vc.location
 	and dt.date = vc.date
-where dt.location = 'Greece' 
+--where dt.location = 'Greece' 
 )
 Select *,	(Total_Vaccinations - VaccinationsIncrement) as Difference_Vaccination
 From VPOP;
 
 
-/* Saving data by Creating Views for further use in visualizations
-*/
+
+--==================================================================================
+/* Saving data by Creating Views for further use in visualizations */
+--==================================================================================
+
 
 ---========================================== Perc_Total_People_Vaccinated ==============================
 DROP VIEW if exists Perc_Total_People_Vaccinated;
@@ -472,6 +580,7 @@ where dt.continent <> ''
 Select *, ROUND((Total_People_vaccinated/Population)*100,4) as Perc_Total_People_vaccinated, 
 			ROUND((Total_People_Fully_vaccinated/Population)*100,4) as Perc_Total_People_Fully_vaccinated 
 From VPOP;
+
 
 
 
